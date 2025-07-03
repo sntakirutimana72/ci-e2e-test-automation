bc03670c7e8ffd3c24beddcb637f12610d0197bc
Steve <sntakirutimana72@gmail.com>
2025-07-03 17:50:14 +0200

    refactor: Add more configuration Enable logging with logback Configure allure listeners for success and failure Update testng.xml

---
+++ src/main/java/com/util/SystemLogger.java
@@ -0,0 +1,12 @@
+package com.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SystemLogger {
+  private SystemLogger() {}
+
+  public static Logger getLogger(Class<?> clazz) {
+    return LoggerFactory.getLogger(clazz);
+  }
+}
