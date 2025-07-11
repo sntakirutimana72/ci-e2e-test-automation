f167a065f8f69c6f17c71b1e8985cb6402bc088d
Steve <sntakirutimana72@gmail.com>
2025-07-11 09:57:30 +0200

    Add more test data

--- src/test/java/data/pojo/posts/PostDataPojo.java
+++ src/test/java/data/pojo/posts/PostDataPojo.java
@@ -3,6 +3,8 @@ package data.pojo.posts;
 import java.util.List;
 
 public record PostDataPojo(List<CreatePojo> create,
+                           List<CreatePojo> create422,
                            List<UpdatePojo> update,
+                           List<UpdatePojo> update404,
                            List<RetrievalOrDeletionPojo> delete,
                            RetrievePojo retrieve) {}
